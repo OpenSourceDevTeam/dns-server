611be43061c47b531f369acffb429308b10695f7
juku <kuenzel.justin@t-online.de>
2018-12-06 23:05:54 +0100

    added interface for Resolver.

---
+++ src/main/java/com/opensdt/dense/resolver/Resolver.java
@@ -0,0 +1,16 @@
+package com.opensdt.dense.resolver;
+
+import io.netty.handler.codec.dns.DnsRecord;
+
+public interface Resolver {
+
+    /**
+    * resolve domain name
+     *
+     * @param question dns question
+     *
+     * @return result ANSWER dns record or null, if domain coulnd't be resolved (e.q. if it doesn't exists in this resolver)
+    */
+    public DnsRecord resolve (DnsRecord question);
+
+}
---
+++ src/main/java/com/opensdt/dense/resolver/ResolverPipeline.java
@@ -0,0 +1,41 @@
+package com.opensdt.dense.resolver;
+
+import com.carrotsearch.hppc.ObjectArrayList;
+import com.carrotsearch.hppc.cursors.ObjectCursor;
+import io.netty.handler.codec.dns.DnsRecord;
+
+public class ResolverPipeline {
+
+    protected ObjectArrayList<Resolver> resolverList = new ObjectArrayList<>();
+
+    /**
+     * resolve domain name
+     *
+     * @param question dns question
+     *
+     * @return result ANSWER dns record or null, if domain coulnd't be resolved (e.q. if it doesn't exists in this resolver)
+     */
+    public DnsRecord resolve (DnsRecord question) {
+        //iterate through pipeline
+        for (ObjectCursor<Resolver> cursor : this.resolverList) {
+            DnsRecord result = cursor.value.resolve(question);
+
+            if (result == null) {
+                continue;
+            }
+
+            return result;
+        }
+
+        return null;
+    }
+
+    public void addResolver (Resolver resolver) {
+        this.resolverList.add(resolver);
+    }
+
+    public void removeResolver (Resolver resolver) {
+        this.resolverList.removeFirst(resolver);
+    }
+
+}
