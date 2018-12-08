f60ef92d1579076af22681043be52d0514d4fb09
Philip <n0rz3@protonmail.com>
2018-12-09 00:52:48 +0100

    Improve code quality and add copyright header

--- src/main/java/com/opensdt/dense/resolver/ResolverPipeline.java
+++ src/main/java/com/opensdt/dense/resolver/ResolverPipeline.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2018 OpenSourceDevTeam
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.opensdt.dense.resolver;
 
 import com.carrotsearch.hppc.ObjectArrayList;
@@ -6,17 +22,16 @@ import io.netty.handler.codec.dns.DnsRecord;
 
 public class ResolverPipeline {
 
-    protected ObjectArrayList<Resolver> resolverList = new ObjectArrayList<>();
+    private ObjectArrayList<Resolver> resolverList = new ObjectArrayList<>();
 
     /**
-     * resolve domain name
+     * Resolves the domain queried in the question section.
      *
-     * @param question dns question
+     * @param question The DNS question section record.
      *
-     * @return result ANSWER dns record or null, if domain coulnd't be resolved (e.q. if it doesn't exists in this resolver)
+     * @return Returns the DNS answer section record or null, if the domain couldn't be resolved (e.q. if it doesn't exists in this resolver).
      */
     public DnsRecord resolve(DnsRecord question) {
-        // try all resolvers and return the first one which can handle the request
         for (ObjectCursor<Resolver> cursor : this.resolverList) {
             DnsRecord result = cursor.value.resolve(question);
 
